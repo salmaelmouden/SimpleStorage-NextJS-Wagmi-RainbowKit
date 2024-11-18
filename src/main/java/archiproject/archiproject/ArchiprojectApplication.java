e22c32e5b3cc023791ea54e35a5cde184c4a73df
Salma EL MOUDEN <salma.el.mouden@etu.imt-nord-europe.fr>
2024-11-18 14:54:16 +0100

    commit1

---
+++ src/main/java/archiproject/archiproject/ArchiprojectApplication.java
@@ -0,0 +1,13 @@
+package archiproject.archiproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArchiprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArchiprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/archiproject/archiproject/ArchiprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package archiproject.archiproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArchiprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
